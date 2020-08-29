061e24fbf71cacd83045de0dca4d3061f225fb60
Melanie <melanie@Tims-MBP.localdomain>
2020-08-29 17:04:50 -0400

    gatsby build

---
+++ public/component---src-pages-add-to-memorial-js-daeeac428ee9a57c3506.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{NsKa:function(e,a,t){"use strict";t.r(a);var l=t("q1tI"),n=t.n(l),o=t("y6RJ"),r=t("Bl7J"),c=t("vrFN");o.a.database();a.default=function(){return n.a.createElement(r.a,null,n.a.createElement(c.a,{title:"Add To Memorial Page"}),n.a.createElement("h1",null,"Pet Memorial"),n.a.createElement("p",null," This page will be devoted to your pets.  Check back soon for more details."))}}}]);
+//# sourceMappingURL=component---src-pages-add-to-memorial-js-daeeac428ee9a57c3506.js.map
