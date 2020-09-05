9017b5fdb5f280c9d25905fef61e6326314a3600
Melanie <melanie@Tims-MBP.localdomain>
2020-09-05 19:28:25 -0400

    updates for memorial

---
+++ gatsby-browser.js
@@ -0,0 +1,11 @@
+/**
+ * Implement Gatsby's Browser APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/browser-apis/
+ */
+
+// You can delete this file if you're not using it
+import "firebase/app"
+import "firebase/auth"
+import "firebase/storage"
+import "firebase/database"
