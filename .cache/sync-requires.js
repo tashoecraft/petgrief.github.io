de5677f21279021522ec6fb27225ef560f63583f
Melanie <melanie@Tims-MBP.localdomain>
2020-10-05 14:43:05 -0400

    gatsby build

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/melanie/Desktop/Development/petgrief.github.io/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/melanie/Desktop/Development/petgrief.github.io/src/pages/404.js"))),
   "component---src-pages-add-pet-info-js": hot(preferDefault(require("/Users/melanie/Desktop/Development/petgrief.github.io/src/pages/addPetInfo.js"))),
   "component---src-pages-add-to-memorial-js": hot(preferDefault(require("/Users/melanie/Desktop/Development/petgrief.github.io/src/pages/addToMemorial.js"))),
